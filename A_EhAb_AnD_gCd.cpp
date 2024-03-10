0aaefe95e5d2f0035e0e38a550bd8c4770b87d4a
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-03-11 01:54:15 +0600

    solved

--- A_EhAb_AnD_gCd.cpp
+++ A_EhAb_AnD_gCd.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+void arif()
+{
+    int n;
+    cin >> n;
+
+    cout << "1 " << n - 1 << endl;
+}
 int main()
 {
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        arif();
+    }
 }
