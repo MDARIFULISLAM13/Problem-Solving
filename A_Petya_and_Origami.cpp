92d238d13d6e40ad4b0fd233e2cb16271aebfa59
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-03-19 23:19:36 +0600

    solve

--- A_Petya_and_Origami.cpp
+++ A_Petya_and_Origami.cpp
@@ -1,2 +1,24 @@
 #include <bits/stdc++.h>
-using n
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int ans = 0;
+    ans += (2 * n) / k;
+    if ((2 * n) % k != 0)
+    {
+        ans += 1;
+    }
+    ans += (5 * n) / k;
+    if ((5 * n) % k != 0)
+    {
+        ans += 1;
+    }
+    ans += (8 * n) / k;
+    if ((8 * n) % k != 0)
+    {
+        ans += 1;
+    }
+    cout << ans;
+}
