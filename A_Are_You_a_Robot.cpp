862fa3127ef82a84eaef5e4ccd777d0fc14de276
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-04-01 00:19:38 +0600

    solve

---
+++ A_Are_You_a_Robot.cpp
@@ -0,0 +1,23 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "security\n";
+    return 0;
+}
---
+++ B_Was_it_Rated.cpp
@@ -0,0 +1,32 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+    return 0;
+}
