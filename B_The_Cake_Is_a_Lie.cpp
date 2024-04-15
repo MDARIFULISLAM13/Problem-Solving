054ccbd65fe97109b90d30e1d6506405ce6dc4ee
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-04-15 21:07:19 +0600

    solved

---
+++ B_The_Cake_Is_a_Lie.cpp
@@ -0,0 +1,41 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+void arif()
+{
+    int n, m, k;
+    cin >> n >> m >> k;
+    if ((n * m) - 1 == k)
+    {
+        cout << "YES\n";
+        return;
+    }
+    cout << "NO\n";
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        arif();
+    }
+
+    return 0;
+}
--- loop.cpp
+++ loop.cpp
@@ -1,57 +1,21 @@
-/**
- *
- * Author : Md.Ariful Islam
- *
- **/
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-using ll = long long int;
-#define yes cout << "YES\n";
-#define no cout << "NO\n";
-#define endl "\n";
-#define ft float
-#define du double
-#define ull unsigned long long
-#define vec vector<ll>
+
+int factorial(int n)
+{
+    // I know this, so I don't want my function to go any further...
+    if (n == 0)
+        return 1;
+    // don't bother what to do, just reuse the function...
+    else
+        return n * factorial(n - 1);
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n;
-    cin >> n;
-    int count = 0;
-    int ck = 0;
-    for (int i = 0; i <= n; i++)
-    {
-        if (ck == 1)
-        {
-            count++;
-            if (count == 4)
-            {
-                count = 0;
-                ck = 0;
-            }
-        }
-        else
-        {
-
-            if (i < 10)
-            {
-                cout << "0";
-            }
-            cout << i << " ";
-            count++;
-            if (count == 6)
-            {
-                cout << endl;
-                count = 0;
-                ck = 1;
-            }
-        }
-    }
-
+    int num;
+    cout << "Enter a number to find its factorial: ";
+    cin >> num;
+    cout << "Factorial of " << num << " is " << factorial(num) << endl;
     return 0;
 }
