2734b1420f3e22823fa24481e0b638ecb336de93
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-04-20 01:44:51 +0600

    solved

---
+++ B_Lovely_Palindromes.cpp
@@ -0,0 +1,33 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    cout << n;
+    while (n > 0)
+    {
+        cout << n % 10;
+        n /= 10;
+    }
+
+    return 0;
+}
