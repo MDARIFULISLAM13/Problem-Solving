9da462116a46e19b352142e304966a6e99c06ba2
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-05-20 02:26:03 +0600

    solved

--- A_Saitama_Destroys_Hotel.cpp
+++ A_Saitama_Destroys_Hotel.cpp
@@ -20,24 +20,7 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int na, nb;
-    cin >> na >> nb;
-    int a, b;
-    cin >> a >> b;
-    int A[na], B[nb];
-    for (int i = 1;i <= na;i++) {
-        cin >> A[i];
-    }
-    for (int i = 1;i <= nb;i++) {
-        cin >> B[i];
-    }
-    if (A[a] >= B[nb + 1 - b]) {
 
-        cout << "NO\n";
-        return 0;
-    }
-
-    cout << "YES\n";
 
     return 0;
 }
