cc3be0b6a7194d3940ce2e503d02bc5fb56361fa
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-05-22 00:26:28 +0600

    solved

---
+++ A_System_of_Equations.cpp
@@ -0,0 +1,26 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+
+    return 0;
+}
