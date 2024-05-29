c5c9907a1f66b26156d505a8b30d2d3886449a7f
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2024-05-30 04:58:28 +0600

    solve

--- B_Restore_the_Weather.cpp
+++ B_Restore_the_Weather.cpp
@@ -13,38 +13,38 @@ using ll = long long int;
 #define du double
 #define ull unsigned long long
 #define vec vector<ll>
-
 void arif() {
     int n, k;
     cin >> n >> k;
     vec a(n);
     vec b(n);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < n; i++) cin >> b[i];
-
+    for (int i = 0;i < n;i++) {
+        cin >> a[i];
+    }
+    for (int i = 0;i < n;i++) {
+        cin >> b[i];
+    }
     vec c(a.begin(), a.end());
     sort(c.begin(), c.end());
     sort(b.begin(), b.end());
-
-    unordered_map<ll, vector<int>> indexMap;
-    for (int i = 0; i < n; i++) {
-        indexMap[c[i]].push_back(i);
+    unordered_map<ll, vector<int>>mp;
+    for (int i = 0;i < n;i++) {
+        mp[c[i]].push_back(i);
     }
-
-    vec result(n);
-    for (int i = 0; i < n; i++) {
-        int j = indexMap[a[i]].back();
-        indexMap[a[i]].pop_back();
-        result[i] = b[j];
+    vec res(n);
+    for (int i = 0;i < n;i++) {
+        int j = mp[a[i]].back();
+        mp[a[i]].pop_back();
+        res[i] = b[j];
     }
-
-    for (int i = 0; i < n; i++) {
-        cout << result[i] << " ";
+    for (int i : res) {
+        cout << i << " ";
     }
     cout << endl;
-}
 
-int main() {
+}
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
