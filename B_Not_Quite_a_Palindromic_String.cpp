1baab9b8d04bf0c3b2721fed869d766d2e570eda
MDARIFULISLAM13 <mdarifulislamncs13@gmail.com>
2025-05-27 21:13:31 +0600

    solve

---
+++ B_Not_Quite_a_Palindromic_String.cpp
@@ -0,0 +1,44 @@
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 27-05-2025
+ * time : 21:11:53
+ * Problem Name : B_Not_Quite_a_Palindromic_String
+ *
+ **/
+#ifdef __GNUC__
+#pragma GCC optimize("O3")
+#pragma GCC target("sse4")
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n";
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+#define mem(dp, i) memset(dp, i, sizeof(dp));
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    s
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
