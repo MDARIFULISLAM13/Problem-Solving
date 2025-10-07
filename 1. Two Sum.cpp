faf570228a8b6c0cbbe4c526249a455b1cdc0f34
Md. Ariful Islam <mdarifulislamncs13@gmail.com>
2025-10-07 12:43:45 -0700

    ok

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,22 +1,6 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+int main(){
 
-class Solution
-{
-public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        for (int i = 0; i < nums.size(); i++)
-        {
-            int x = target - nums[i];
-            auto it = find(nums.begin(), nums.end(), x);
-            if (it != nums.end() && it - nums.begin() != i)
-            {
-                int y = distance(nums.begin(), it);
-                return {i, y};
-            }
-        }
-
-        return {};
-    }
-};
+    
+}
