e1b34ac3c861e9ad4693f805db5956c954f407ad
Md. Ariful Islam <mdarifulislamncs13@gmail.com>
2026-08-30 17:43:30 +0600

    solve

--- a.cpp
+++ a.cpp
@@ -1,34 +1,67 @@
-#include <stdio.h>
-#include <math.h>
+/**
+ *
+ * Author : Md.Ariful Islam
+ * Date : 2026-08-30
+ * Time : 17:05:51
+ * Problem Name : a
+ *
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl "\n"
+#define ft float
+#define du double
+#define ull unsigned long long
+#define vec vector<ll>
+#define rev(a) reverse(a.begin(), a.end());
+#define sort(a) sort(a.begin(), a.end());
+#define mem(dp, i) memset(dp, i, sizeof(dp));
 
-int hasZero(int n)
+void solve()
 {
-    int digits = (int)log10(n) + 1; 
-
-    while (digits--)
+    ll n;
+    cin >> n;
+    vec v(n), vv(n);
+    map<ll, ll> mp;
+    for (int i = 0; i < n; i++)
     {
-        if (n % 10 == 0)
-            return 1;
-        n /= 10;
+        cin >> v[i];
+        mp[v[i]]++;
+        vv[i] = v[i];
     }
+    sort(vv);
+    rev(vv);
 
-    return 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] != vv[0])
+        {
+            cout << v[i] * vv[0] << " ";
+        }
+        else
+        {
+            if (mp[v[i]] > 1)
+            {
+                cout << v[i] * vv[0] << " ";
+            }
+            else
+            {
+                cout << v[i] * vv[1] << " ";
+            }
+        }
+    }
 }
 
 int main()
 {
-    int n;
-    scanf("%d", &n);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-    long long sum = 0;
-
-    for (int i = 1; i <= n; i++)
-    {
-        if (!hasZero(i))
-            sum += i;
-    }
-
-    printf("%lld\n", sum);
+    solve();
 
     return 0;
 }
